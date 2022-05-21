4bf5228911be173141398afd0fd9a19ab45c18a0
tscRubbish <2010075010@qq.com>
2022-05-21 18:09:37 +0800

    补充代码，readme，ppt

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # NJUSE_CV_HomeWork1
+
+## 作业目录
+
+- element：人脸装饰的组件图片，内有图片处理程序，需要用处理程序element_process.py将目标组件处理成target.png
+- originPic：原图
+- resultPic：处理结果
+
+## 作业文件
+
+- Pic_face.py：对指定图片的人脸进行标注，并识别标注人脸关键点
+- video_face.py：对视频的人脸进行标注，并识别标注人脸关键点
+- whiten.py：对图片人脸进行磨皮和美白
+- video_beauty：对视频人脸进行磨皮
+- face_thin：图片人脸瘦脸
+- facePaint.py：简单的图片数字化妆(口红)
+- decorate.py：图片的人脸装饰(发饰，眼镜)
+- video_decorate.py：视频的人脸装饰
+- haarcascade_frontalface_default.xml：默认的正脸级联分类器
+- shape_predictor_68_face_landmarks.dat：人脸68关键点模型
+
+## 代码依赖
+
+主要环境Python3.8(部分代码不是)
+
+主要依赖如下：
+
+- opencv 4.5.5
+- face_recognition 1.3.0
+- dlib 19.21.0
+- scipy 
+
+可通过如下pip命令安装opencv，face_recognition,dlib图形库
+
+```
+pip install python
+pip install dlib-bin
+pip install face_recognition
+```
+
+如果pip install face_recognition报错，可能是需要安装cmake依赖
+
+```
+pip install cmake
+```
+
+## 代码运行
+
+如果想更换处理图片，请在代码中找到图片\视频输入路径变量，修改输入路径
+
+同理，更换输出路径，请在代码中找到图片\视频输出路径变量，修改输出路径
+
+## 注意
+
+代码均经过本地调试运行，如果有依赖相关报错或者程序相关报错，请助教联系QQ：2010075010或邮箱2010075010@qq.com，以尽量避免不必要的误会。
+
---
+++ whiten.py
@@ -0,0 +1,55 @@
+Color_list = [
+    1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 31, 33, 35, 37, 39,
+    41, 43, 44, 46, 48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 66, 67, 69, 71, 73, 74,
+    76, 78, 79, 81, 83, 84, 86, 87, 89, 91, 92, 94, 95, 97, 99, 100, 102, 103, 105,
+    106, 108, 109, 111, 112, 114, 115, 117, 118, 120, 121, 123, 124, 126, 127, 128,
+    130, 131, 133, 134, 135, 137, 138, 139, 141, 142, 143, 145, 146, 147, 149, 150,
+    151, 153, 154, 155, 156, 158, 159, 160, 161, 162, 164, 165, 166, 167, 168, 170,
+    171, 172, 173, 174, 175, 176, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187,
+    188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203,
+    204, 205, 205, 206, 207, 208, 209, 210, 211, 211, 212, 213, 214, 215, 215, 216,
+    217, 218, 219, 219, 220, 221, 222, 222, 223, 224, 224, 225, 226, 226, 227, 228,
+    228, 229, 230, 230, 231, 232, 232, 233, 233, 234, 235, 235, 236, 236, 237, 237,
+    238, 238, 239, 239, 240, 240, 241, 241, 242, 242, 243, 243, 244, 244, 244, 245,
+    245, 246, 246, 246, 247, 247, 248, 248, 248, 249, 249, 249, 250, 250, 250, 250,
+    251, 251, 251, 251, 252, 252, 252, 252, 253, 253, 253, 253, 253, 254, 254, 254,
+    254, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+    255, 255, 255, 256]
+
+import cv2
+from PIL import Image, ImageEnhance
+
+def face_whitening(fileName):
+    img = cv2.imread(fileName)
+    img = cv2.bilateralFilter(img, 19, 75, 75)
+    height, width, n = img.shape
+    img2 = img.copy()
+    for i in range(height):
+        for j in range(width):
+            b = img2[i, j, 0]
+            g = img2[i, j, 1]
+            r = img2[i, j, 2]
+            img2[i, j, 0] = Color_list[b]
+            img2[i, j, 1] = Color_list[g]
+            img2[i, j, 2] = Color_list[r]
+    cv2.imwrite("whiten1.jpg",img2)
+
+    image = Image.open("whiten1.jpg")
+    # 锐度调节
+    enh_img = ImageEnhance.Sharpness(image)
+    image_sharped = enh_img.enhance(1.2)
+    # 颜色均衡调节
+    con_img = ImageEnhance.Contrast(image_sharped)
+    image_con = con_img.enhance(1.2)
+    image_con.save("resultPic/whiten2.jpg")
+
+    # img1 = cv2.imread("58.jpg")
+    # img2 = cv2.imread("whiten2.jpg")
+    # cv2.imshow("1", img1)
+    # cv2.imshow("2", img2)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    face_whitening("originPic/04.jpg")
